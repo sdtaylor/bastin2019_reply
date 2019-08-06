9f22eeb1d807d4ec1e28f841ccd8558c1b3411d4
Shawn <shawntaylor@weecology.org>
2019-08-06 10:23:33 -0400

    add protected area status in sampled points
    from the IUCN Database of Protected areas
    protectedplanet.net

--- compile_carbon_stock_data.R
+++ compile_carbon_stock_data.R
@@ -5,6 +5,7 @@ base_data_folder = '/home/shawn/data/global_biomass_and_treecover/'
 
 ############################################
 ecoregions = rgdal::readOGR(paste0(base_data_folder,'wwf_terrestrial_biomes'),'wwf_terr_ecos')
+protected_areas = rgdal::readOGR(paste0(base_data_folder,'protected_areas'),'WDPA_Aug2019-shapefile-polygons')
 
 aboveground_biomass_raster_files = list.files(paste0(base_data_folder,'aboveground_carbon') ,pattern = 'aboveground_biomass_ha_2000', full.names = T)
 tree_cover_raster_files = list.files(paste0(base_data_folder,'tree_cover') ,pattern = 'Hansen_GFC', full.names = T)
@@ -20,10 +21,15 @@ points_spatial = SpatialPointsDataFrame(points_spatial, data=data.frame(point_id
 points_df = as_tibble(points_spatial)
 
 ##############################################
-# Get ecoregions for all point
+# Get ecoregions and protected areas for all points
 points_df$biome_id =sp::over(points_spatial, ecoregions)$BIOME
 
+protected_areas_info = sp::over(points_spatial, protected_areas)
+points_df$protected_area_status = protected_areas_info$IUCN_CAT
 
+# Save the full protected areas metadata for all sampled points
+protected_areas_info$point_id = points_df$point_id
+write_csv(protected_areas_info,'protected_area_info_for_random_points.csv')
 ##############################################
 
 final_data = tibble()
