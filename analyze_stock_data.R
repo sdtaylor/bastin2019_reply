d4eef163eea0f3a403f8e09ae72b550701b5607b
Shawn <shawntaylor@weecology.org>
2019-08-08 13:58:58 -0400

    clean up comments, get rid of marine stuff

--- analyze_stock_data.R
+++ analyze_stock_data.R
@@ -36,7 +36,7 @@ bastin_carbon = bastin_carbon %>%
     TRUE ~ biomass
   ))
 
-# Pretty naems for the different carbon pools
+# Pretty names for the different carbon pools
 base_levels = c('above_ground_carbon','soc','total_carbon')
 nice_names = c('Above Ground Carbon','Soil Organic Carbon','Total Carbon')
 bastin_carbon$biomass_type = factor(bastin_carbon$biomass_type, levels = base_levels,labels = nice_names)
@@ -47,7 +47,6 @@ biomass_and_cover$biomass_type = factor(biomass_and_cover$biomass_type, levels =
 large_plot_biomes = c('Boreal Forests/Taiga','Tropical Grasslands, Savannas & Shrublands',
                       'Temperate Broadleaf & Mixed Forests', 'Deserts & Xeric Shrublands',
                       'Tropical Moist Broadleaf Forests','Temperate Grasslands, Savannas & Shrublands')
-
 
 large_plot = ggplot(filter(biomass_and_cover,biome_name %in% large_plot_biomes), aes(x=tree_cover, y=biomass)) + 
   geom_point(alpha=0.5, color='grey60', shape=1) + 
@@ -66,7 +65,7 @@ large_plot = ggplot(filter(biomass_and_cover,biome_name %in% large_plot_biomes),
   labs(x='Percent Tree Cover',y='Tonnes C/Ha')
 ggsave('manuscript/fig1_carbon_stock_plots.png', plot = large_plot, width = 20, height = 26, units = 'cm', dpi=400)
 
-# the plot again but all biomes
+# the large plot again but all biomes
 large_plot_all_biomes = ggplot(biomass_and_cover, aes(x=tree_cover, y=biomass)) + 
   geom_point(alpha=0.5, color='grey60', shape=1) + 
   #scale_fill_viridis_c() + 
@@ -84,7 +83,7 @@ large_plot_all_biomes = ggplot(biomass_and_cover, aes(x=tree_cover, y=biomass)) 
   labs(x='Percent Tree Cover',y='Tonnes C/Ha')
 ggsave('manuscript/fig2_carbon_stock_plots_all_biomes.png', plot = large_plot_all_biomes, width = 20, height = 60, units = 'cm', dpi=300)
 
-# Just total carbon and a single biome
+# Just total carbon and a single biome for diagnostic plots
 focal_biome = 'Temperate Grasslands, Savannas & Shrublands'
 biomass_and_cover %>%
   filter(biome_name == focal_biome) %>%
@@ -93,13 +92,8 @@ biomass_and_cover %>%
   stat_poly_eq(aes(label =  paste(stat(eq.label), stat(adj.rr.label), sep = "~~~~")),
                formula = y~x, rr.digits = 3, coef.digits = 2, parse = TRUE,
                size=5) + 
-  #scale_fill_viridis_c() + 
-  #geom_smooth(color='#0072B2') +
   geom_smooth(method = 'lm', color = 'black', size=3) + 
   geom_line(data = filter(bastin_carbon,biome_name ==focal_biome), color='#D55E00', size=3) + 
-  #scale_y_continuous(breaks=seq(0,500,100)) + 
-  #ylim(0,500) + 
-  #coord_cartesian(ylim=c(0,500)) + 
   facet_wrap(biome_name~biomass_type , scales = 'free_y', ncol=3) +
   theme_bw(base_size=25) +
   labs(x='% Tree Cover', y='Tonnes C/Ha')
@@ -107,7 +101,6 @@ biomass_and_cover %>%
 ########################################################
 # Calculate the estimate of slopes from Figure 1
 biome_coefficients = biomass_and_cover %>%
-  #sample_frac(0.3) %>%
   group_by(biome_name, biomass_type) %>%
   do(tidy(lm(biomass ~ tree_cover, data = .))) %>%
   ungroup() %>%
@@ -129,27 +122,6 @@ biome_coefficients = biome_coefficients %>%
 
 biome_coefficients$biome_name = forcats::fct_reorder(biome_coefficients$biome_name, -biome_coefficients$carbon_coef)
 
-ggplot(biome_coefficients, aes(x=str_wrap(biome_name, 30), y=carbon_coef * 100, fill=source)) +
-  #geom_errorbarh(data = filter(biome_coefficients, source=='Current Estimates'),  
-  #               aes(xmin = (carbon_coef - (carbon_coef_std_error*1.96)) * 100, 
-  #               xmax = (carbon_coef + (carbon_coef_std_error*1.96))*100),
-  #               height=0.5, color='grey20', size=1) +
-  #geom_point(size=5) +
-  geom_col(position = position_dodge(width = 0.5), width = 0.5) + 
-  scale_fill_manual(values = c('#D55E00','black')) + 
-  #xlim(-5,5) + 
-  #geom_vline(xintercept = 0, size=1) + 
-  theme_bw() +
-  theme(axis.text = element_text(size=15),
-        axis.title = element_text(size=20),
-        axis.text.x = element_text(angle=60, hjust = 1, debug = F),
-        panel.grid = element_line(color='grey60'),
-        legend.position = c(0.4, 0.8),
-        legend.text = element_text(size=20),
-        legend.title = element_blank(),
-        legend.background = element_rect(color='grey40')) +
-  labs(y = 'Tonnes carbon stock potential added\n for every Ha of canopy cover', x='')
-
 ##################################################
 bastin_tree_potential = read_csv('bastin_restoration_potential.csv')
 
@@ -164,7 +136,7 @@ biome_carbon_potential = biome_coefficients %>%
 
 
 ##################################
-# Table
+# Table 1 data
 table_data = biome_carbon_potential %>%
   select(-restoration_potential_Mha) %>%
   gather(value_type, value, T_C_ha_potential, GtC_potential) %>%
@@ -172,28 +144,3 @@ table_data = biome_carbon_potential %>%
   mutate(col_header = paste(biomass_type, value_type, source)) %>%
   select(-biomass_type, -value_type, -source) %>%
   spread(col_header, value) 
-
-write_csv(table_data, 'derived_data/table_data.csv')
-# %>%
-#   #filter(!biome_name %in% c('Boreal Forests/Taiga','Tundra')) %>%
-#   group_by(source, biomass_type) %>%
-#   summarise(global_total = sum(GtC_potential))
-
-
-# figure of uncertainty of total carbon estimated with current data
-biome_coefficients %>%
-  group_by(biome_name, biomass_type, source) %>%
-  do(tibble(carbon_coef = rnorm(1000, mean = .$carbon_coef, sd = .$carbon_coef_std_error),
-            bootstrap = 1:1000)) %>%
-  ungroup() %>%
-  left_join(bastin_tree_potential, by='biome_name') %>%
-  mutate(GtC_potential = ((carbon_coef*100 * 10**6) * restoration_potential_Mha)/10**9) %>%
-  filter(!biome_name %in% c('Boreal Forests/Taiga','Tundra')) %>%
-  group_by(source, bootstrap) %>%
-  summarise(global_total = sum(GtC_potential)) %>%
-  filter(source=='the data') %>%
-ggplot(aes(global_total, color=source)) + 
-  geom_density()
-
-  
-#################################
